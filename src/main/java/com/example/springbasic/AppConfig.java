a68798ad6b189bff42b30bc5c0dede3b8334c60e
junyeong.jo <whwnsdud2024@naver.com>
2023-06-13 09:49:44 +0900

    스프링으로 전환하기

--- src/main/java/com/example/springbasic/AppConfig.java
+++ src/main/java/com/example/springbasic/AppConfig.java
@@ -9,35 +9,32 @@ import com.example.springbasic.member.MemberServiceImpl;
 import com.example.springbasic.member.MemoryMemberRepository;
 import com.example.springbasic.order.OrderService;
 import com.example.springbasic.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author junyeong.jo .
  * @since 2023-06-12
  */
+@Configuration
 public class AppConfig {
-    /**
-     * AppConfig는 애플리케이션의 실제 동작에 필요한 "구현 객체를 생성" 한다.
-     * MemberServiceImpl
-     * MemoryMemberRepository
-     * OrderServiceImpl
-     * FixDiscountPolicy
-     *
-     * AppConfig는 생성한 객체 인스턴스의 참조(래퍼런스)를 생성자를 통해서 주입 해준다.
-     */
+
+    @Bean
     public MemberService memberService () {
         return new MemberServiceImpl(memberRepository());
     }
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
-
-
 }
