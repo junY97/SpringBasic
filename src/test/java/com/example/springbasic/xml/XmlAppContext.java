ea6ee513ddeef20b1d4eba8e2efe43d05560a3de
junyeong.jo <whwnsdud2024@naver.com>
2023-06-16 10:47:27 +0900

    다양한 설정 형식 지원 - 자바 코드, XML

---
+++ src/test/java/com/example/springbasic/xml/XmlAppContext.java
@@ -0,0 +1,21 @@
+package com.example.springbasic.xml;
+
+import com.example.springbasic.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+/**
+ * @author junyeong.jo .
+ * @since 2023-06-16
+ */
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
