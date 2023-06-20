59be1e85e7ed2d2658f8d1f6e26968121b2a926c
junyeong.jo <whwnsdud2024@naver.com>
2023-06-20 17:06:04 +0900

    다양한 의존관계 주입 방법

--- src/test/java/scan/AutoAppConfigTest.java
+++ src/test/java/scan/AutoAppConfigTest.java
@@ -1,7 +1,9 @@
 package scan;
 
 import com.example.springbasic.AutoAppConfig;
+import com.example.springbasic.member.MemberRepository;
 import com.example.springbasic.member.MemberService;
+import com.example.springbasic.order.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -19,5 +21,9 @@ public class AutoAppConfigTest {
 
         MemberService memberService = ac.getBean(MemberService.class);
         Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+
+        OrderServiceImpl bean = ac.getBean(OrderServiceImpl.class);
+        MemberRepository memberRepository = bean.getMemberRepository();
+        System.out.println("memberRepository = " + memberRepository);
     }
 }
