cc15a3ae44d60e5c45454a2b11caa50c19e55104
junyeong.jo <whwnsdud2024@naver.com>
2023-06-23 15:34:32 +0900

    스코프와 Provider

--- src/main/java/com/example/springbasic/web/LogDemoController.java
+++ src/main/java/com/example/springbasic/web/LogDemoController.java
@@ -1,9 +1,9 @@
 package com.example.springbasic.web;
 
 import com.example.springbasic.common.MyLogger;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,11 +16,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class LogDemoController  {
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider; // ObjectProvider 덕분에 빈의 생성 지연가능
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject(); // request 요청이 오면 스프링 컨테이너로 부터 객체 요청
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
--- src/main/java/com/example/springbasic/web/LogDemoService.java
+++ src/main/java/com/example/springbasic/web/LogDemoService.java
@@ -1,6 +1,8 @@
 package com.example.springbasic.web;
 
 import com.example.springbasic.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,14 +10,12 @@ import org.springframework.stereotype.Service;
  * @since 2023-06-23
  */
 @Service
+@RequiredArgsConstructor
 public class LogDemoService {
-    private final MyLogger myLogger;
-
-    public LogDemoService(MyLogger myLogger) {
-        this.myLogger = myLogger;
-    }
+    private final ObjectProvider<MyLogger> myLoggerProvider; // ObjectProvider 덕분에 빈의 생성 지연가능
 
     public void logic(String id) {
+        MyLogger myLogger = myLoggerProvider.getObject(); // request 요청이 오면 스프링 컨테이너로 부터 객체 요청
         myLogger.log("service id = " + id);
     }
 }
